e8a2689ce8fd3151ff6a683e5817bc68771ab805
UHarishinkx <your-email@example.com>
2026-04-18 13:35:37 +0530

    UC20 - Linear search with exception with 8 tests

---
+++ App/src/main/UseCase20TrainConsistMgmt.java
@@ -0,0 +1,9 @@
+public class UseCase20TrainConsistMgmt {
+
+    public static int search(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] == key) return i;
+
+        throw new RuntimeException("Not Found");
+    }
+}
